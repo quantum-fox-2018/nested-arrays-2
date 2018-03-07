e3d52ee1f38447db113cebd7c5db192fb8bf434e
Zena <zena.vilenia@gmail.com>
2018-03-07 11:46:44 +0700

    tugas nested array 2 convert zena

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -7,20 +7,32 @@ let roster = [
   [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 25] ],
   [7, 'Sally Talls', 'Power Forward ', [18, 29, 26, 31, 19] ],
   [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0] ]
-]
+];
 
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
 function convert_roster_format (nestedArray) {
   // your convert code here
+  let arrHasil = [];
+  let obj = {};
+  for(let i = 1; i < nestedArray.length; i++) {
+    obj = {};
+    obj[nestedArray[0][0]] = nestedArray[i][0];
+    obj[nestedArray[0][1]] = nestedArray[i][1];
+    obj[nestedArray[0][2]] = nestedArray[i][2];
+    obj[nestedArray[0][3]] = nestedArray[i][3];
+    arrHasil.push(obj);
+  }
+
+  return arrHasil;
 }
 
-let object_roster = convert_roster_format(roster)
-console.log(object_roster[2])
+let object_roster = convert_roster_format(roster);
+console.log(object_roster[2]);
 
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
 
-console.log(object_roster[0]['Name'] == 'Joe Schmo') // outputs true
+console.log(object_roster[0]['Name'] == 'Joe Schmo'); // outputs true
 
 module.exports = {
   convert_roster_format
