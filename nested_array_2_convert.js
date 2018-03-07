ac0ec54ec3b7621f199adb208cdaf1a81a4dd3d9
ekifakhrureza <ekifakhrureza@gmail.com>
2018-03-07 15:02:11 +0700

    Nested Array Jilid 2

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -2,17 +2,29 @@
 
 let roster = [
   ['Number', 'Name', 'Position', 'Points per Game'],
-  [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23] ],
-  [9, 'Ms. Buckets', 'Point Guard', [19, 0, 11, 22, 0] ],
-  [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 25] ],
-  [7, 'Sally Talls', 'Power Forward ', [18, 29, 26, 31, 19] ],
-  [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0] ]
+  [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23]],
+  [9, 'Ms. Buckets', 'Point Guard', [19, 0, 11, 22, 0]],
+  [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 25]],
+  [7, 'Sally Talls', 'Power Forward ', [18, 29, 26, 31, 19]],
+  [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0]]
 ]
 
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
-function convert_roster_format (nestedArray) {
+function convert_roster_format(nestedArray) {
   // your convert code here
+  let newArr = []
+
+  for (var i = 1; i < nestedArray.length; i++) {
+    let obj = {}
+    obj[roster[0][0]] = nestedArray[i][0]
+    obj[roster[0][1]] = nestedArray[i][1]
+    obj[roster[0][2]] = nestedArray[i][2]
+    obj[roster[0][3]] = nestedArray[i][3]
+    newArr.push(obj)
+  }
+
+  return newArr
 }
 
 let object_roster = convert_roster_format(roster)
