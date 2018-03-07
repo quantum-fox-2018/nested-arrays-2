deb2f5488c8a8141d3b2cfe02edb323222c6392b
yasirmaulana <yasir.maulana@gmail.com>
2018-03-07 16:49:25 +0700

    day 3, nested array 2 convert

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -1,7 +1,8 @@
 'use strict'
 
 let roster = [
-  ['Number', 'Name', 'Position', 'Points per Game'],
+  //['Number', 'Name', 'Position', 'Points per Game'],
+  ['Number', 'Name', 'Position', 'pointsPerGame'],
   [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23] ],
   [9, 'Ms. Buckets', 'Point Guard', [19, 0, 11, 22, 0] ],
   [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 25] ],
@@ -11,8 +12,21 @@ let roster = [
 
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
-function convert_roster_format (nestedArray) {
-  // your convert code here
+function convert_roster_format (roster) {
+  let users = []
+  let keylist = roster[0]
+  for(let i=1; i < roster.length; i++){
+    let user = {}
+    // user[keylist[0]] = roster[i][0]
+    // user[keylist[1]] = roster[i][1]
+    // user[keylist[2]] = roster[i][1]
+    // user[keylist[3]] = roster[i][2]
+    for(let j=0; j<keylist.length; j++){
+      user[keylist[j]] = roster[i][j]
+    }
+    users.push(user)
+  }
+  return users
 }
 
 let object_roster = convert_roster_format(roster)
